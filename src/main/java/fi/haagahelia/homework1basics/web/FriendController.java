df0e8f89cdb585f15bea8c1d58622336ce8f876f
MohammadHanifArghushi <mha.lafg@gmail.com>
2025-08-31 17:38:09 +0300

    3.Friend List part (b) is done, adding a new html for adding new friends to the existing list

--- src/main/java/fi/haagahelia/homework1basics/web/FriendController.java
+++ src/main/java/fi/haagahelia/homework1basics/web/FriendController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import fi.haagahelia.homework1basics.domain.Friend;
 
 @Controller
@@ -24,5 +27,17 @@ public class FriendController {
 
         model.addAttribute("friends", friends);
         return "friendlist";
+    }
+
+    @GetMapping("/add")
+    public String showAddFriendForm(Model model) {
+        model.addAttribute("friend", new Friend());
+        return "addfriend";
+    }
+
+    @PostMapping("/add")
+    public String saveFriend(@ModelAttribute Friend friend) {
+        friends.add(friend);
+        return "redirect:/index";
     }
 }
