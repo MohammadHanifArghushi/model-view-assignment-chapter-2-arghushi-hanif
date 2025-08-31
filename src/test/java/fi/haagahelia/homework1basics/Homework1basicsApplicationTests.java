633325dca54a44179773aec43df661551ba86e58
MohammadHanifArghushi <mha.lafg@gmail.com>
2025-08-31 15:41:33 +0300

    Exercise 1: HelloController with /hello?name&age

---
+++ src/test/java/fi/haagahelia/homework1basics/Homework1basicsApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.homework1basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework1basicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
